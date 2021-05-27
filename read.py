1e0ba8f423663dbdfd196547091149f9c40b8708
Aiden Jhang <ssjhang0510@gmail.com>
2021-05-27 16:47:20 +0800

    add progressbar

--- read.py
+++ read.py
@@ -1,11 +1,14 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0: # %用來求餘數 ,讀一千筆才印出一次
-			print(len(data))
+		bar.update(count)
 print('檔案讀取完了，總共有',len(data), '筆資料')
 print(data[0])
 
@@ -33,11 +36,11 @@ print('一共有', len(good), '筆留言提到good')
 print(good[0])
 
 #list comprehension 清單快寫法 
-good = [1 for d in data if 'good' in d]
-print(good)
+good = ['wow' for d in data if 'good' in d]
+# print(good)
 
 bad = ['bad' in d for d in data]
-print(bad)
+# print(bad)
 
 #以上快寫法相等於:
 bad = []
@@ -46,6 +49,7 @@ for d in data:
 
 
 #文字計數
+start_time = time.time()
 wc = {} #word_count
 for d in data:
 	words = d.split()  #spli預設空白建
@@ -57,6 +61,8 @@ for d in data:
 for word in wc:
 	if wc[word] > 1000000:
 		print(word, wc[word])
+end_time = time.time()
+print('花了', end_time - start_time, 'seconds')
 print(len(wc))
 print(wc['Allen'])
 
